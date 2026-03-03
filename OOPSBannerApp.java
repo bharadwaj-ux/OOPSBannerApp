f4205e0a125510c53241f777a470167b2abbcbc3
bharadwaj-ux <vn9789@srmist.edu.in>
2026-03-03 14:52:37 +0530

    feature/uc3

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,13 +2,20 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****    *****    ******    *****  ");
-        System.out.println("*     *  *     *   *     *  *     * ");
-        System.out.println("*     *  *     *   *     *  *       ");
-        System.out.println("*     *  *     *   ******    *****  ");
-        System.out.println("*     *  *     *   *              * ");
-        System.out.println("*     *  *     *   *        *     * ");
-        System.out.println(" *****    *****    *         *****  ");
+        String[] banner = {
 
+            " *****    *****    *****    *****  ",
+            "*     *  *     *  *     *  *       ",
+            "*     *  *     *  *     *  *       ",
+            "*     *  *     *  *****     *****  ",
+            "*     *  *     *  *               *",
+            "*     *  *     *  *               *",
+            " *****    *****    *         *****  "
+
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
